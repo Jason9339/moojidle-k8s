ba1478213b0aa50a37b8f44291554d3ab0a21967
benny999 <benny999.yu@gmail.com>
2025-05-04 19:49:16 +0800

    [Style][Chore][Feat] add .env.example, add style framework example, change all code to indent 4

---
+++ project/frontend/src/components/LeftBar/LeftBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { leftBarNavItems } from "@/constants/LeftBarNavItems";
+import { Link } from "react-router-dom";
+
+import './LeftBar.css'
+
+function LeftBar() {
+    return (
+        <Sidebar>
+            <Menu className="menu">
+                {leftBarNavItems.map((item) => {
+                    return <MenuItem key={item.path} icon=<item.icon /> 
+                        component={<Link to={item.path} className="link" />}>{item.label}
+                    </MenuItem>
+                })}
+            </Menu>
+        </Sidebar>
+    );
+}
+
+export default LeftBar;
