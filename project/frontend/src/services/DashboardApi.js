24d01afbd234b24f2f0d44771ab2e793ec6a7bea
FWNT <fwnt35808319@gmail.com>
2025-05-05 17:14:45 +0800

    fix: 修正前端 API 請求地址設置，解決 mock 資料 404 錯誤問題

--- project/frontend/src/services/DashboardApi.js
+++ project/frontend/src/services/DashboardApi.js
@@ -1,17 +1,17 @@
-import apiClient, { mockApiClient } from "./apiClient";
+import axios from "./apiClient";
 
 export const getCourses = async () => {
-  return (await mockApiClient.get("/mock/courses.json")).data;
+  return (await axios.get("/mock/courses.json")).data;
 };
 
 export const getTodoList = async () => {
-  return (await mockApiClient.get("/mock/todo.json")).data;
+  return (await axios.get("/mock/todo.json")).data;
 };
 
 export const getComingUpList = async () => {
-  return (await mockApiClient.get("/mock/comingup.json")).data;
+  return (await axios.get("/mock/comingup.json")).data;
 };
 
 export const getTeachIn = async () => {
-  return (await mockApiClient.get("/mock/teach_in.json")).data;
+  return (await axios.get("/mock/teach_in.json")).data;
 };
