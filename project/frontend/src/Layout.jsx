fa66d371af1bf08430271b8e6ab9a222127d51f1
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-05-07 02:50:58 +0800

    [refactor][chore] fixed leftbar. enable TailwindCSS

--- project/frontend/src/Layout.jsx
+++ project/frontend/src/Layout.jsx
@@ -3,12 +3,12 @@ import LeftBar from "@/components/LeftBar/LeftBar";
 
 function Layout() {
     return (
-        <div style={{ height: "4000px", display: "flex" }}>
+        <div className="flex">
             <LeftBar />
-            <main style={{ flex: 1 }}>
+            <main className="flex-1">
                 <Outlet />
             </main>
-        </div>
+        </div >
     );
 }
 
--- project/frontend/src/components/LeftBar/LeftBar.jsx
+++ project/frontend/src/components/LeftBar/LeftBar.jsx
@@ -2,29 +2,27 @@ import React from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { leftBarNavItems } from "@/constants/LeftBarNavItems";
 import { Link } from "react-router-dom";
-// import './LeftBar.css' to import the styles
-import './test.css'
 
 function LeftBar() {
     return (
-        <Sidebar className="fixed top-0 left-0">
-            <Menu>
-                {leftBarNavItems.map((item) => (
-                    <MenuItem
-                        key={item.path}
-                        icon={<item.icon />}
-                        component={<Link to={item.path} />}
-                        className="py-3 px-5 font-bold"
-                        activeClassName="!text-[#5961d4]"
-                    >
-                        {item.label}
-                    </MenuItem>
-                ))}
-            </Menu>
-            <div className="!bg-transparent">
-                {/* if you have an icon wrapper elsewhere */}
-            </div>
-        </Sidebar>
+
+        <div className="fixed top-0 left-0 z-50 !h-screen">
+            <Sidebar className="!h-full">
+                <Menu>
+                    {leftBarNavItems.map((item) => (
+                        <MenuItem
+                            key={item.path}
+                            icon={<item.icon />}
+                            component={<Link to={item.path} />}
+                            className="py-3 px-5 font-bold"
+                            activeClassName="!text-[#5961d4]"
+                        >
+                            {item.label}
+                        </MenuItem>
+                    ))}
+                </Menu>
+            </Sidebar>
+        </div>
     );
 }
 
