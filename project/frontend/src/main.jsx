2c56dde037750c51c5e658b00b290b50d5ebd039
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-05-06 14:28:34 +0800

    [feat] basic text editor

--- project/frontend/src/main.jsx
+++ project/frontend/src/main.jsx
@@ -7,7 +7,7 @@ import NotFoundPage from "@/pages/NotFoundPage";
 import DiscussionBoard from "@/pages/DiscussionBoard";
 import Dashboard from "@/pages/Dashboard";
 import DiscussionBoardList from "@/pages/DiscussionBoardList";
-
+import GithubCommentEditor from "@/components/GithubCommentEditor" 
 const router = createBrowserRouter([
     {
         path: "/",
@@ -16,12 +16,14 @@ const router = createBrowserRouter([
             { path: "dashboard", element: <Dashboard /> },
             { path: "discussion", element: <DiscussionBoardList /> },
             { path: "discussion/:id", element: <DiscussionBoard /> },
+            { path: "text", element: <GithubCommentEditor />},
             { path: "*", element: <NotFoundPage /> },
         ],
     },
 ]);
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <div className="text-7x1 text-cyan-50">Hello</div>
         <RouterProvider router={router} />
     </StrictMode>
 );
