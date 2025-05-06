c5d9bbcfd0489ab5f7d2e4f3d7bcad3edc1346b4
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-05-07 02:25:36 +0800

    [refactor] add Leftbar.css

--- project/frontend/src/Layout.jsx
+++ project/frontend/src/Layout.jsx
@@ -3,7 +3,7 @@ import LeftBar from "@/components/LeftBar/LeftBar";
 
 function Layout() {
     return (
-        <div style={{ display: "flex" }}>
+        <div style={{ height: "4000px", display: "flex" }}>
             <LeftBar />
             <main style={{ flex: 1 }}>
                 <Outlet />
--- project/frontend/src/components/LeftBar/LeftBar.jsx
+++ project/frontend/src/components/LeftBar/LeftBar.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { leftBarNavItems } from "@/constants/LeftBarNavItems";
 import { Link } from "react-router-dom";
-
 // import './LeftBar.css' to import the styles
+import './test.css'
 
 function LeftBar() {
     return (
-        <Sidebar>
+        <Sidebar className="fixed top-0 left-0">
             <Menu>
-                {leftBarNavItems.map((item) => {
-                    return <MenuItem key={item.path} icon=<item.icon /> 
-                        component={<Link to={item.path} className="link" />}>{item.label}
+                {leftBarNavItems.map((item) => (
+                    <MenuItem
+                        key={item.path}
+                        icon={<item.icon />}
+                        component={<Link to={item.path} />}
+                        className="py-3 px-5 font-bold"
+                        activeClassName="!text-[#5961d4]"
+                    >
+                        {item.label}
                     </MenuItem>
-                })}
+                ))}
             </Menu>
+            <div className="!bg-transparent">
+                {/* if you have an icon wrapper elsewhere */}
+            </div>
         </Sidebar>
     );
 }
