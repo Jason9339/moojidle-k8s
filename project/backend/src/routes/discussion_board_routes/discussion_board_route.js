3f711c67b884b737060e3923befcc2aaf9d1b0e0
nonohuang <yh7624908@email.com>
2025-05-12 03:50:33 +0800

    [Feat]build the board api with allcourseboard, courseboard, add and delete

--- project/backend/src/routes/discussion_board_routes/discussion_board_route.js
+++ project/backend/src/routes/discussion_board_routes/discussion_board_route.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 import { 
     GetCourseDiscussionBoard,
+    GetAllCourseDiscussionBoard,
+    AddDiscussionBoard, 
+    DeleteDiscussionBoard 
 } from '#src/controllers/discussion_board_controllers/discussion_board_controller.js';
 
+router.get("/course-boards/:courseId", GetCourseDiscussionBoard);
+router.get("/user-course-boards/:userId", GetAllCourseDiscussionBoard);
 
-router.get("/course-boards", GetCourseDiscussionBoard);
+router.post("/add-course-boards", AddDiscussionBoard)
 
+router.delete("/course-boards/:boardId", DeleteDiscussionBoard)
 export default router;
