77b3e0d1f27eb34a78b05df5c3ad88060ab66947
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-05-12 14:48:37 +0800

    [feat] enrich comment_by_user_name in backend post_controller

--- project/backend/src/controllers/discussion_board_controllers/post_controller.js
+++ project/backend/src/controllers/discussion_board_controllers/post_controller.js
@@ -2,6 +2,8 @@ import { FindBoardByID } from '#src/services/post_services/discussion_board_serv
 import { FindPostByID } from '#src/services/post_services/post_service.js'
 import { FindUserNameByID } from '#src/services/post_services/user_servcie.js';
 import { FindCourseNameByID } from '#src/services/post_services/course_service.js';
+
+
 async function GetPostContent(req, res, next) {
     try {
         const postId = parseInt(req.params.id);
@@ -11,20 +13,39 @@ async function GetPostContent(req, res, next) {
 
         const postData = await FindPostByID(postId);
         if (!postData) {
-            return res.status(404).send({ error: "Post not found" });
+            return res.status(404).send({ error: "Post Not Found" });
         }
 
         const authorName = await FindUserNameByID(postData.post_by_user_id);
         if (!authorName) {
-            return res.status(404).send({ error: "Author not found" });
+            return res.status(404).send({ error: "Author Not found" });
         }
         postData.author_name = authorName;
 
         const boardData = await FindBoardByID(postData.in_b_id);
+        if (!boardData) {
+            return res.status(404).send({ error: "Discussion Board Not Found" });
+        }
         postData.board_name = boardData.name;
 
         const courseName = await FindCourseNameByID(boardData.course_id);
+
+        if (!courseName) {
+            return res.status(404).send({ error: "Couurse Not Found" });
+        }
         postData.course_name = courseName;
+
+
+        // Missing commentator's name, enrich it.
+        postData.comments = await Promise.all(
+            postData.comments.map(async comment => (
+                {
+                    ...comment,
+                    comment_by_user_name: await FindUserNameByID(comment.comment_by_user_id)
+                }
+            ))
+        )
+
         res.status(200).send(postData);
     } catch (err) {
         next(err);
