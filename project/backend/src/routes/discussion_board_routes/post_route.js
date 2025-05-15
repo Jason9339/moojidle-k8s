2f05be493efd63d660b7dfdb6f26cbadd1cf13ed
shiokko <bowsoul062904@gmail.com>
2025-05-15 21:56:54 +0800

    [Refactor] fix discussion backend controller

--- project/backend/src/routes/discussion_board_routes/post_route.js
+++ project/backend/src/routes/discussion_board_routes/post_route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const postRouter = express.Router();
-import { GetPostContent, Commender, PostDeleter, CommendDeleter } from '#src/controllers/discussion_board_controllers/post_controller.js'
+import { GetPostContent, Commender, PostDeleter, CommendDeleter } from '#src/controllers/discussion_controllers/post_controllers.js'
 
 postRouter.get("/content/:id", GetPostContent);
 postRouter.post("/commend", Commender);
