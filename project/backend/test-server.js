d1e74960e430c503a8e6fd00fb851d0fdf625fcb
zihcilin <jasonlin930309@gmail.com>
2025-05-24 22:04:05 +0800

    [fix] 修正引用路徑

--- project/backend/test-server.js
+++ project/backend/test-server.js
@@ -5,16 +5,16 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 
 // 導入路由
-import userRoute from './src/routes/user_route.js';
-import courseRoute from './src/routes/course_router.js';
-import courseMemberRoute from './src/routes/course_member_route.js';
-import assignmentRoute from './src/routes/assignment_route.js';
-import materialRoute from './src/routes/material_route.js';
-import announcementRoute from './src/routes/announcement_route.js';
-import discussionBoardRoute from './src/routes/discussion_board_route.js';
-import postRoute from './src/routes/post_routes.js';
-import examRoute from './src/routes/exam_route.js';
-import fileRoute from './src/routes/file_routes/file_route.js';
+import userRoute from '#src/routes/user_route.js';
+import courseRoute from '#src/routes/course_router.js';
+import courseMemberRoute from '#src/routes/course_member_route.js';
+import assignmentRoute from '#src/routes/assignment_route.js';
+import materialRoute from '#src/routes/material_route.js';
+import announcementRoute from '#src/routes/announcement_route.js';
+import discussionBoardRoute from '#src/routes/discussion_board_route.js';
+import postRoute from '#src/routes/post_routes.js';
+import examRoute from '#src/routes/exam_route.js';
+import fileRoute from '#src/routes/file_routes/file_route.js';
 
 let mongoServer;
 let server;
