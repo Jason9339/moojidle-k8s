d66e122dc5400b7072fc494b79c1efbda72f683a
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-05-28 21:08:31 +0800

    [feat] won't show events in the past

--- project/frontend/src/services/CalendarApi.js
+++ project/frontend/src/services/CalendarApi.js
@@ -4,17 +4,18 @@ export async function GetCalendarEventsByUserId(userId) {
 
     try {
         const response = await api.get(`/calendar/get-events/${userId}`);
+        const currentDate = new Date();
         const data = response.data.map((courseWithEvents) => {
-            const events = courseWithEvents.events.map(({ title, start, end }) => ({
+            let events = courseWithEvents.events.map(({ title, start, end }) => ({
                 title: title, start: new Date(start), end: new Date(end)
-            }))
+            })
+            );
 
+            events = events.filter(evt => evt.end > currentDate);
 
             return { ...courseWithEvents, events: events };
 
-        }
-        )
-
+        })
 
         return data;
     }
