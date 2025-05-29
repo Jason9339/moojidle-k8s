73d7118eda65c9a8a34297d11ec6196663500833
benny999 <benny999.yu@gmail.com>
2025-05-30 00:58:56 +0800

    [Docs] forget to write comments in router

--- project/backend/src/routes/taken_exams_route.js
+++ project/backend/src/routes/taken_exams_route.js
@@ -7,7 +7,27 @@ import { GetAllStudentsProjectedTakenExam } from '#src/controllers/taken_exams_c
 
 // frontend gives courseId
 // backend gives back submitted-ass for each student
-
+// [
+//     {
+//         "user_id": 1,
+//         "name": "User 1",
+//         "student_id": 3099,
+//         "taken_exams": [
+//             {
+//                 "exam_name": "Exam 1 for Course 1",
+//                 "percentage": 0.1
+//             },
+//             {
+//                 "exam_name": "Exam 2 for Course 1",
+//                 "percentage": 0.1
+//             },
+//             {
+//                 "exam_name": "Exam 3 for Course 1",
+//                 "percentage": 0.1
+//             }
+//         ]
+//     },
+// .............
 router.get("/in-course/:courseId", GetAllStudentsProjectedTakenExam);
 
 export default router;
