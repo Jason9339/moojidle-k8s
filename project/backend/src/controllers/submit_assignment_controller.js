ca5ce0a39c1b9a6dec899cd9c63ae782ffcb0e2d
nonohuang <yh7624908@email.com>
2025-06-03 03:03:00 +0800

    [Refactor] refactor backend APi

--- project/backend/src/controllers/submit_assignment_controller.js
+++ project/backend/src/controllers/submit_assignment_controller.js
@@ -4,7 +4,9 @@ import {
     FindSubmissionAssignmentBySubmitAssId
 } from '#src/services/submit_assignment_service.js';
 
-import { FindCourseIdByAssignmentId, FindAssignmentMaxScore } from '#src/services/assignment_service.js';
+import { 
+    FindAssByAssId 
+} from '#src/services/assignment_service.js';
 
 import { FindStudyInJoinUserByCourseId } from '#src/services/course_member_service.js';
 
@@ -12,16 +14,15 @@ async function GetAssignmentSubmissions(req, res) {
     try {
         const { assignmentId } = req.params;
 
-        console.log(assignmentId)
-
         // Validate input
         if (!assignmentId) {
             return res.status(400).json({ message: "缺少作業ID" });
         }
         const assId = parseInt(assignmentId);
 
+        const ass = await FindAssByAssId(assId); 
         // Get course ID from assignment ID
-        const courseId = await FindCourseIdByAssignmentId(assId); 
+        const courseId = ass.in_course_id; 
         // console.log("courseId", courseId)
         if (!courseId) {
             return res.status(404).json({ message: "找不到對應的課程" });
@@ -98,11 +99,12 @@ async function ReviewAssignmentSubmission(req, res) {
             return res.status(400).json({message: "沒有該繳交作業"})
         }
         
+        const ass = await FindAssByAssId(existingSubmission.ass_id);
 
 
         const score_lb = 0;
         // 獲取作業的最大分數 
-        const score_ub = await FindAssignmentMaxScore (existingSubmission.ass_id);
+        const score_ub = ass.max_score;
         if (score < score_lb || score > score_ub) {
             return res.status(400).json({message: "分數不得超過上限或為負數"})
         }
