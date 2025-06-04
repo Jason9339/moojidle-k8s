631ec1ca8ac72c893cfc133af2a5b1e520747293
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-06-05 00:25:30 +0800

    I forgot to delete console.log

--- project/frontend/src/components/VerticalScrollIndicator/VerticalScrollIndicator.jsx
+++ project/frontend/src/components/VerticalScrollIndicator/VerticalScrollIndicator.jsx
@@ -42,7 +42,6 @@ export default function VerticalOverflowIndicator({
         }
     }, []);
 
-    console.log("show shadow:", showBottomShadow);
     useEffect(() => {
         checkShadows();
     }, [checkShadows]);
