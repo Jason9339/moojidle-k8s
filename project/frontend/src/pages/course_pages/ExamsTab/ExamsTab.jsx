7a3484526900f3496ae3fdf3aaa676d130a7cca9
EricLiu750501 <qazwsxedc123456789eric@gmail.com>
2025-06-07 01:06:50 +0800

    [fix] Tab display

--- project/frontend/src/pages/course_pages/ExamsTab/ExamsTab.jsx
+++ project/frontend/src/pages/course_pages/ExamsTab/ExamsTab.jsx
@@ -12,17 +12,10 @@ function ExamsTab() {
             const data = await GetCourseExams(course.courseId);
             setTeacherExams(data || []);
         }
-        if (role.isTeacher || role.isAssistant) {
             fetchTeacherExams();
-        }
     }, [course.courseId, role]);
 
-    if(role.isStudent){
-        return <div>ExamsTab for Student</div>;
-    }
-    else{
-        return <TeacherExam exams = {TeacherExams}/>;
-    }
+    return <TeacherExam exams = {TeacherExams}/>;
 }
 
 export default ExamsTab;
